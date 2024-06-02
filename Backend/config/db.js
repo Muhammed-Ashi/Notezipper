92d4a383111adb370424aa488cfb58da0709dc93
Muhammed Ashiq <muhammedashiq176@gmail.com>
2024-06-02 20:11:14 +0530

    Update data base name

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ const Mongoose = require('mongoose')
 
 module.exports = {
     DB_Connection: () => {
-        Mongoose.connect('mongodb://127.0.0.1:27017/noteZipper', function (err) {
+        Mongoose.connect('mongodb://127.0.0.1:27017/noteZipper_db', function (err) {
             if (err) {
                 console.log(err)
             }
